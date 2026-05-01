0511bdd10ecbb75c5f05cd50ad39f83c4e2f5ea8
joytivism <joytivism@gmail.com>
2026-05-01 09:36:04 +0700

    feat: remove header navbar from client dashboard layout

--- src/app/(client)/layout.tsx
+++ src/app/(client)/layout.tsx
@@ -1,12 +1,10 @@
 import Sidebar from "@/components/Sidebar";
-import Header from "@/components/Header";
 
 export default function ClientLayout({ children }: { children: React.ReactNode }) {
   return (
     <>
       <Sidebar />
       <div className="flex-1 ml-[248px] flex flex-col min-h-screen bg-bg">
-        <Header />
         <main className="flex-1 p-6 lg:p-8">
           {children}
         </main>
